6530a079e77b9d27a7b03d37bb58651982e895e8
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2019-10-09 14:27:38 +0200

    ejercicio idiomas practica y traducir el reto 0 empezado

---
+++ reto0/Temporada3.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/estilo3.css">
+	<title>Stranger Things</title>
+
+</head>
+<body>
+	<header>
+		<a href="index.php"><h1>Stranger Things</h1></a>
+	</header>
+	<div id="todo">
+	<nav>
+		<ul>
+		  <li>
+		    <a href="Temporada1.php">
+		      Temporada 1
+		    </a>
+		  </li>
+		  <li>
+		    <a href="temporada2.php">
+		      Temporada 2
+		    </a>
+		    </li>
+		  <li>
+		    <a href="Temporada3.php">
+		      Temporada 3
+		    </a>
+		  </li>
+		   <li>
+		  	<a href="reparto.php">
+		  		Reparto
+		  	</a>
+		  </li>
+		</ul>
+	</nav>
+	<section>
+		
+		<div id="video">
+			<iframe width="330" height="197" src="https://www.youtube.com/embed/uHZ5JskXw4I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	</section>
+	<article id="descripcion1">
+			<p>
+				Un romance en ciernes. Un nuevo centro comercial. Ratas rabiosas que corren hacia el peligro. Es el verano de 1985 en Hawkins. Y un verano puede cambiarlo todo.
+			</p>
+		</article>
+		<article id="episodios1">
+			<h3>Episodios:</h3>
+			<ol>
+				<li>Suzie, ¿me recibes?</li>
+				<li>Ratas de centro comercial</li>
+				<li>El caso de la socorrista desaparecida</li>
+				<li>La prueba de la sauna</li>
+				<li>El ejército del Azotamentes</li>
+				<li>E pluribus unum</li>
+				<li>La mordedura</li>
+				<li>La batalla de Starcourt</li>
+				
+			</ol>
+		</article>
+	<aside>
+		<img src="imagenes/suda.jpg" id="merch">
+		<a id="pop" href="https://www.beautifulhalo.com/white-long-sleeve-letter-stranger-things-printed-pullover-loose-hoodie-p-376702.html?language_type=es&curSize=M&showColor=White&currency1=EUR&track=PA_G_SESWC&gclid=EAIaIQobChMI3b-z6fzZ5AIVTdTeCh3_gwmJEAkYFSABEgK7G_D_BwE" target="blank"><b>20.04€</b><br>White Long Sleeve Letter Stranger Things - <br>Printed Pullover Loose Hoodie</a>
+
+	</aside>
+
+
+
+	
+	<footer>
+		<h2>Stranger Things - Andoni &nbsp;Bartolome, &nbsp;Xabier Jacob &nbsp;y &nbsp;Aitor Ortiz de Zarate <?php echo(strftime("%d/%m/%Y %H:%M")); ?>
+			<a href="https://twitter.com/Stranger_Things?ref_src=twsrc%5Etfw"class="twitter-follow-button"data-show-count="false">Follow</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></h2>
+	</footer>
+	</div>
+
+</body>
+</html>
---
+++ reto0/temporada2.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/csstemp2.css">
+	<title>Stranger Things</title>
+
+</head>
+<body>
+	<header>
+		<a href="index.php"><h1>Stranger Things</h1></a>
+	</header>
+	<div id="todo">
+	<nav>
+		<ul>
+		  <li>
+		    <a href="Temporada1.php">
+		      Temporada 1
+		    </a>
+		  </li>
+		  <li>
+		    <a href="temporada2.php">
+		      Temporada 2
+		    </a>
+		    </li>
+		  <li>
+		    <a href="Temporada3.php">
+		      Temporada 3
+		    </a>  
+		  </li>
+		   <li>
+		  	<a href="reparto.php">
+		  		Reparto
+		  	</a>
+		  </li>
+		</ul>
+	</nav>
+	<section>
+		
+		<div id="video">
+			<iframe width="330" height="197" src="https://www.youtube.com/embed/knLy7MtfHHw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	</section>
+	<article id="descripcion1">
+			<p>
+				Ha pasado casi un año desde la misteriosa desaparición de Will. Pero la vida no ha vuelto a la normalidad en Hawkins. Ni por asomo.
+			</p>
+		</article>
+		<article id="episodios1">
+			<h3>Episodios:</h3>
+			<ol>
+				<li>MADMAX</li>
+				<li>Truco o trato, bicho raro</li>
+				<li>El renacuajo</li>
+				<li>Will el Sabio</li>
+				<li>Dig Dug</li>
+				<li>El espía</li>
+				<li>La hermana perdida</li>
+				<li>El Azotamentes</li>
+				<li>El portal</li>
+			</ol>
+		</article>
+	<aside>
+		<img src="imagenes/cami.jpg" id="merch">
+		<a id="pop" href="https://www.google.com/aclk?sa=L&ai=DChcSEwjmo6et_9nkAhUO2t4KHURvDMwYABA6GgJ3Yg&sig=AOD64_1fPbOH4Ao88csy840e0q-exOPR0g&ctype=5&q=&ved=0ahUKEwj0g6St_9nkAhUPohQKHSNwDmMQ1CkIjAY&adurl=" target="blank"><b>€ 4,22 - 4,69</b><br>ARTIST Camiseta -<br>Stranger Things</a>
+
+	</aside>
+
+
+
+	
+	<footer>
+		<h2>Stranger Things - Andoni Bartolome, Xabier Jacob y Aitor Ortiz de Zarate <?php echo(strftime("%d/%m/%Y %H:%M")); ?>
+			<a href="https://twitter.com/Stranger_Things?ref_src=twsrc%5Etfw"class="twitter-follow-button"data-show-count="false">Follow</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></h2>
+	</footer>
+	</div>
+
+</body>
+</html>
